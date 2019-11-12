04b4d385205f9f326c0c55ad9b2e5116ba6af52c
Leandro Andreaci <leandroandreaci@hotmail.com>
2019-11-12 10:55:53 -0300

    add phpunit

---
+++ tests/ExampleTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Leandreaci\Tests;
+
+
+use Illuminate\Http\Request;
+
+class ExampleTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $this->assertTrue(true);
+    }
+}
---
+++ tests/TestCase.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Leandreaci\Tests;
+
+class TestCase extends \PHPUnit\Framework\TestCase
+{
+
+}
